f691c995ea99011dcc70a954f19d583a597151a9
Sheena Patel <sheenapatel2013@gmail.com>
2016-07-09 16:44:59 -0400

    So check out Account..java
    For whatever reason when a string of two numbers w/ different lengths (e.g. 10 and 11) is inputted, with the length of 11 beginning with 1 being first,
     the code is disregarding the 1 being the first character. Idk if it is just syntax or what

--- app/src/main/java/com/catalizeapp/catalize_ss25/Account.java
+++ app/src/main/java/com/catalizeapp/catalize_ss25/Account.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Account extends AppCompatActivity {
 
@@ -22,7 +24,88 @@ public class Account extends AppCompatActivity {
         final TextView people = (TextView) findViewById(R.id.people);
         String temp = Contacts.people;
         temp = temp.replaceAll("[^0-9]","");
-        people.setText(temp.substring(0,10));
+        temp = temp.trim();
+
+        if (temp.length()==14){ //this is for numbers of length 7 w/o area code (ex: 471-9427)
+            String person1 = temp.substring(0,7);
+            String person2 = temp.substring(7);
+            Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 7 & 7", Toast.LENGTH_LONG).show();
+        }
+
+       else if (temp.length() == 17){ //this is for a number of len 7 and a number of len 10
+            String temp2 = Contacts.people;
+            temp2 = temp2.replaceAll("[^0-9,-]","");
+            temp2 = temp2.trim();
+            int index = temp2.indexOf("-");
+            String check = temp2.substring(index+1,index+5);
+            if (check.contains("-")){
+                temp2 = temp2.replaceAll("[^0-9]","");
+                String person1 = temp2.substring(0,10);
+                String person2 = temp2.substring(10);
+
+                Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 10 and 7", Toast.LENGTH_LONG).show();
+            }
+
+            else{
+                temp2 = temp2.replaceAll("[^0-9]","");
+                String person1 = temp2.substring(0,7);
+                String person2 = temp2.substring(7);
+                Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 7 and 10", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        else if (temp.length()==18){ //for numbers of length 7 and 11
+            String firstNum = temp.substring(0,1); //this is not working properly. only works when first number is 7 digits long
+            //nothing going into this if statement below
+            if (firstNum.compareTo("1")==1){
+                String person1 = temp.substring(0,11);
+                String person2 = temp.substring(11);
+                Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 11 and 7", Toast.LENGTH_LONG).show();
+            }
+            else{
+                String person1 = temp.substring(0,7);
+                String person2 = temp.substring(7);
+                Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 7 and 11", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+        else if (temp.length()==20){
+            String person1 = temp.substring(0,10);
+            String person2 = temp.substring(10);
+            Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 20", Toast.LENGTH_LONG).show();
+        }
+        else if ( temp.length()== 21) { //this is not working properly. only works when first number is 10 digits long
+            //nothing going into this if statement below
+            String firstNum = temp.substring(0,1);
+            if (firstNum.compareTo("1")==1){
+                String person1 = temp.substring(0, 11);
+                String person2 = temp.substring(11);
+                Toast.makeText(getApplicationContext(), person1+" "+person2 + "length 21 #1", Toast.LENGTH_LONG).show();
+            }
+            else {
+                String person1 = temp.substring(0, 10);
+                String person2 = temp.substring(10);
+                String index = temp.substring(0);
+                Toast.makeText(getApplicationContext(),person1 + " " + person2 + "length 21 #2", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        else if (temp.length() == 22) {
+            String person1 = temp.substring(0, 11);
+            String person2 = temp.substring(11);
+            Toast.makeText(getApplicationContext(), person1+" "+person2 +"length 22", Toast.LENGTH_LONG).show();
+        }
+
+        else if (temp.length() > 22){
+            Toast.makeText(getApplicationContext(), "Please only select two people to introduce", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Toast.makeText(getApplicationContext(), "One of these numbers is invalid", Toast.LENGTH_SHORT).show();
+        }
+
+        //people.setText(temp.substring(0,10));
         //sendIntent.putExtra(et.getText().toString(), "default content");
         //sendIntent.setType("vnd.android-dir/mms-sms");
 
@@ -31,7 +114,7 @@ public class Account extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    SmsManager.getDefault().sendTextMessage("2013751471", null, et.getText().toString()+ "\n" + Contacts.people, null, null);
+                    SmsManager.getDefault().sendTextMessage("9154719427", null, et.getText().toString()+ "\n" + Contacts.people, null, null);
                 } catch (Exception e) {
                     AlertDialog.Builder alertDialogBuilder = new
                             AlertDialog.Builder(Account.this);
