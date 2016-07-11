497bdb264d43faa5ce109e45214f612c7a8999ea
meenall <meenalk01@gmail.com>
2016-07-11 18:31:13 -0400

    bug RESOLVED: selecting contacts without searching for them Email contacts imported Rename dialog popup for introduction between email contacts Dialog for introduction sent

--- app/src/main/java/com/catalizeapp/catalize_ss25/Account.java
+++ app/src/main/java/com/catalizeapp/catalize_ss25/Account.java
@@ -1,13 +1,14 @@
 package com.catalizeapp.catalize_ss25;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,26 +18,117 @@ import android.widget.Toast;
 public class Account extends AppCompatActivity {
 
     Context context = null;
-    private String personName;
-    private SharedPreferences sharedPreferences;
-    private String personEmail;
+    boolean flag = false;
+    private String result;
+    private String result2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
         context = this;
-        sharedPreferences = this.getSharedPreferences("com.catalizeapp.catalize_ss25", Context.MODE_PRIVATE);
 
-        EditText prompt = (EditText) findViewById(R.id.prompt);
-        prompt.setText("Hello " + Contacts.person1 + ", meet " + Contacts.person2 + ". I am introducing you two because...");
+        //final Dialog dialog = new Dialog(Account.this);
+        final EditText prompt = (EditText) findViewById(R.id.prompt);
+        if (Contacts.person1.contains("@") && Contacts.person2.contains("@")) {
+            TextView name = (TextView) findViewById(R.id.nameerror);
+            //name.setText("Set a name for " + Contacts.person1 + ": ");
+            LayoutInflater li = LayoutInflater.from(context);
+            View promptsView = li.inflate(R.layout.layout_prompts, null);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    context);
+
+            alertDialogBuilder.setView(promptsView);
+
+            final EditText userInput = (EditText) promptsView
+                    .findViewById(R.id.editTextDialogUserInput);
+            final EditText userInput2 = (EditText) promptsView
+                    .findViewById(R.id.input2);
+
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    flag = true;
+                                    // get user input and set it to result
+                                    // edit text
+                                    result = userInput.getText().toString();
+                                    result2 = userInput2.getText().toString();
+                                    prompt.setText("Hello " + result + ", meet " + result2 + ". I am introducing you two because...");
+                                    //Contacts.person1 = result;
+                                }
+                            })
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+        }
+
+        if (Contacts.person1.contains("@") && !Contacts.person2.contains("@") || Contacts.person2.contains("@") && !Contacts.person1.contains("@")) {
+            LayoutInflater li = LayoutInflater.from(context);
+            View promptsView = li.inflate(R.layout.layout_prompt, null);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    context);
+
+            alertDialogBuilder.setView(promptsView);
+
+            final EditText userInput = (EditText) promptsView
+                    .findViewById(R.id.editTextDialogUserInput);
+
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    flag = true;
+                                    // get user input and set it to result
+                                    // edit text
+                                    result = userInput.getText().toString();
+                                    if (Contacts.person1.contains("@")) {
+                                        prompt.setText("Hello " + result + ", meet " + Contacts.person2 + ". I am introducing you two because...");
+                                    } else {
+                                        prompt.setText("Hello " + Contacts.person1 + ", meet " + result + ". I am introducing you two because...");
+                                    }
+                                    //Contacts.person1 = result;
+                                }
+                            })
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+        }
+
+
+
 
         final Intent sendIntent = new Intent(Intent.ACTION_VIEW);
         final EditText et=(EditText)findViewById(R.id.prompt);
         final TextView people = (TextView) findViewById(R.id.people);
         //people.setText(Contacts.people);
 
-       /* Contacts.numbers = Contacts.numbers.replaceAll("[^0-9]","");
+        Contacts.numbers = Contacts.numbers.replaceAll("[^0-9]","");
         String temp = Contacts.numbers;
         if (temp.length()==14){ //this is for numbers of length 7 w/o area code (ex: 471-9427)
             String person1 = temp.substring(0,7);
@@ -106,9 +198,9 @@ public class Account extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Please only select two people to introduce", Toast.LENGTH_SHORT).show();
         }
         else {
-            Toast.makeText(getApplicationContext(), "One of these numbers is invalid", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), "One of these numbers is invalid", Toast.LENGTH_SHORT).show();
         }
-*/
+
         //people.setText(temp.substring(0,10));
         //sendIntent.putExtra(et.getText().toString(), "default content");
         //sendIntent.setType("vnd.android-dir/mms-sms");
@@ -120,14 +212,35 @@ public class Account extends AppCompatActivity {
         //sendIntent.setType("vnd.android-dir/mms-sms");
 
         Button send = (Button) findViewById(R.id.send);
-        personEmail= sharedPreferences.getString("email","");
-        personName = sharedPreferences.getString("name","");
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    SmsManager.getDefault().sendTextMessage("9154719427", null, "Introduction from " + personName + " " + personEmail+ " " + "Contacts: " + Contacts.numbers + "\n" + et.getText().toString(), null, null);
-                    finish();
+                    SmsManager.getDefault().sendTextMessage("9154719427", null, "Contacts: " + Contacts.numbers + "\n" + et.getText().toString(), null, null);
+                    LayoutInflater li = LayoutInflater.from(context);
+                    View promptsView = li.inflate(R.layout.sent, null);
+
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            context);
+
+                    alertDialogBuilder.setView(promptsView);
+
+                    alertDialogBuilder
+                            .setCancelable(false)
+                            .setPositiveButton("Ok",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            // get user input and set it to result
+                                            // edit text
+                                            finish();
+                                        }
+                                    });
+
+                    // create alert dialog
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+
+                    // show it
+                    alertDialog.show();
                 } catch (Exception e) {
                     AlertDialog.Builder alertDialogBuilder = new
                             AlertDialog.Builder(Account.this);
@@ -142,19 +255,6 @@ public class Account extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-
-        switch(item.getItemId()){
-            case(R.id.menu_1):
-                Intent intentReportBug = new Intent(Account.this, ReportBug.class); //
-                startActivity(intentReportBug);
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
 
